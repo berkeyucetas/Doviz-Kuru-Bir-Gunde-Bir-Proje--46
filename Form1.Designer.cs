479638a33c99064414b1ed6b489cc7e90eee338b
Berke <116177776+berkeyucetas@users.noreply.github.com>
2023-11-20 20:58:41 +0300

    Doviz-Kuru-Bir-Günde-Bir-Proje--46
    Bugün yaptığım projede doların euronun ve sterlinin günlük kurunu gösteriyor

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Döviz_Kuru
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.a = new System.Windows.Forms.Label();
+            this.b = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // a
+            // 
+            this.a.AutoSize = true;
+            this.a.Location = new System.Drawing.Point(104, 9);
+            this.a.Name = "a";
+            this.a.Size = new System.Drawing.Size(56, 16);
+            this.a.TabIndex = 0;
+            this.a.Text = "Dövizler";
+            // 
+            // b
+            // 
+            this.b.AutoSize = true;
+            this.b.Location = new System.Drawing.Point(201, 9);
+            this.b.Name = "b";
+            this.b.Size = new System.Drawing.Size(70, 16);
+            this.b.TabIndex = 1;
+            this.b.Text = "Günlük Kur";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(13, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(12, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 188);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 42);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Döviz Kurları";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(428, 238);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.b);
+            this.Controls.Add(this.a);
+            this.Name = "Form1";
+            this.Text = "DÖVİZ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label a;
+        private System.Windows.Forms.Label b;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Döviz_Kuru
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string url = "https://kur.doviz.com/serbest-piyasa/amerikan-dolari.xml";
+
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode(); 
+
+                    string xmlString = await response.Content.ReadAsStringAsync();
+
+                    XDocument xmlDoc = XDocument.Parse(xmlString);
+                    DateTime tarih = DateTime.Parse(xmlDoc.Root.Attribute("Tarih").Value);
+
+                    string DOLAR = xmlDoc.Descendants("Currency")
+                                       .First(c => c.Attribute("Kod").Value == "USD")
+                                       .Element("BanknoteSelling")
+                                       .Value;
+                    label1.Text = $"Tarih: {tarih.ToString()} USD: {DOLAR}";
+
+                    string EURO = xmlDoc.Descendants("Currency")
+                                       .First(c => c.Attribute("Kod").Value == "EUR")
+                                       .Element("BanknoteSelling")
+                                       .Value;
+                    label2.Text = $"Tarih: {tarih.ToString()} EUR: {EURO}";
+
+                    string POUND = xmlDoc.Descendants("Currency")
+                                       .First(c => c.Attribute("Kod").Value == "GBP")
+                                       .Element("BanknoteSelling")
+                                       .Value;
+                    label3.Text = $"Tarih: {tarih.ToString()} GBP: {POUND}";
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                MessageBox.Show("HTTP isteği sırasında hata oluştu: " + ex.Message);
+                label1.Text = "Tarih= 20.11.2023 - Dolar Kuru: 28,7499   ";
+                label2.Text = "Tarih= 20.11.2023 - Euro Kuru: 31,4455   ";
+                label3.Text = "Tarih= 20.11.2023 - Pound Kuru: 35,9561   ";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Beklenmeyen bir hata oluştu: " + ex.Message);
+                label1.Text = "Tarih= 20.11.2023 - Dolar Kuru: 28,7499   ";
+                label2.Text = "Tarih= 20.11.2023 - Euro Kuru: 31,4455   ";
+                label3.Text = "Tarih= 20.11.2023 - Pound Kuru: 35,9561   ";
+            }
+        }
+    }
+}
